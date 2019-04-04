946b42601aeda8cb18a0b8577049f2100551453e
Layth Holubeshen gov <layth.holubeshen@gov.bc.ca>
2019-04-04 10:04:28 -0700

    Adding New Course

---
+++ ISAPublic/story_content/user.js
@@ -0,0 +1,48 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "69eJmK7JXtJ":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date())-1293900000;
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  return {
+    'total': t,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    player.SetVar("hours",('0' + t.hours).slice(-2));
+    player.SetVar("minutes",('0' + t.minutes).slice(-2));
+    player.SetVar("seconds",('0' + t.seconds).slice(-2));
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv', deadline);
+}
+
